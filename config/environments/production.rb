37ad2e7e60f7a837100291196268de27ecefa65e
madebyeggs <madebyeggs@gmail.com>
2017-01-30 23:36:09 +0000

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,32 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.paperclip_defaults = {
-    :s3_protocol => :https,
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
-  config.action_mailer.default_url_options = { :host => 'beetle-tv.herokuapp.com' }
   #Rails.application.routes.default_url_options[:host] = 'beetle-tv.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-  }
 
 end
