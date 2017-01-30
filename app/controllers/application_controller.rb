431b57082b6ca9f3a2fe34de1914d336001ed865
madebyeggs <madebyeggs@gmail.com>
2017-01-30 23:34:09 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def bring_in_models
-    @works = Work.rank(:row_order).all
-    @contact = Contact.new
   end
     
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
   end
   
 end
